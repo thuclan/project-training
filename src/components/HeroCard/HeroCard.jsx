f359adb96dd7468ba872e6d610284392d01a0197
thuclan <lanbd1998@gmail.com>
2022-03-28 20:15:53 +0700

    update herocard

--- src/components/HeroCard/HeroCard.jsx
+++ src/components/HeroCard/HeroCard.jsx
@@ -3,16 +3,20 @@ import './herocard.css';
 
 import frame from '../../static/images/frame.png';
 
+const info = {
+  text: 'Chào mừng đến ',
+  title: 'Diễn đàn sinh viên Đại học Sư phạm kỹ thuật TP.HCM',
+  detail:
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse necessitatibus in qui fuga, beatae modi recusandae est perspiciatis exercitationem ipsum!',
+};
+
 const HeroCard = () => (
   <div className="hero">
     <img src={frame} alt="laptop" />
     <div className="hero__content">
-      <h2>Chào mừng đến </h2>
-      <h1>Diễn đàn sinh viên Đại học Sư phạm kỹ thuật TP.HCM</h1>
-      <h3>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit . A dolor, amet placeat minus
-        voluptatibus corrupti quos illum modi quasi maxime?
-      </h3>
+      <h2>{info.text}</h2>
+      <h1>{info.title}</h1>
+      <h3>{info.detail}</h3>
     </div>
   </div>
 );
