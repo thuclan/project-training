0d25de7f19a7d903d20d7ff07687b9192a109ce9
thuclan <lanbd1998@gmail.com>
2022-03-30 23:30:55 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './css/normalize.css';
 import Header from './components/Header/Header';
 import HeroCard from './components/HeroCard/HeroCard';
+import Trending from './components/Trending/Trending';
+import Footer from './components/Footer/Footer';
 
 const App = () => (
   <>
     <Header />
-    <HeroCard />
+    <main>
+      <HeroCard />
+    </main>
+    <Footer />
   </>
 );
 export default App;
