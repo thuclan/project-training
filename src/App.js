51112ad7407baded3d6007fb354a25ea0802e45a
thuclan <lanbd1998@gmail.com>
2022-04-16 16:52:05 +0700

    add New component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import HeroCard from './components/HeroCard/HeroCard';
 import Footer from './components/Footer/Footer';
 import ScrollToTopButton from './components/ScrollToTopButton/ScrollToTopButton';
 import Trending from './components/Trending/Trending';
+import New from './components/New/New';
 
 const App = () => (
   <>
@@ -12,6 +13,7 @@ const App = () => (
     <main>
       <HeroCard />
       <Trending />
+      <New />
       <ScrollToTopButton />
     </main>
     <Footer />
